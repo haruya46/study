6d6eecaa7bb760d5f7d6f05575bbfa1ec220a1bb
seiya-Unity <pazu70@gmail.com>
2022-05-26 00:24:30 +0900

    0.003

---
+++ abc.php
@@ -0,0 +1,42 @@
+<?php
+//クラス作成
+class test{
+    //プロパティの作成
+    public $name;
+    //nameプロパティを作成
+    //　↓（__construct(引数を受け取ることが出来る)←コンストラクタメソット　インスタンスが作成されたときに必ず呼び出すことが出来る
+    public function __construct($age){
+        //この中に処理を書く
+        echo $age;
+    }
+
+
+    function text(){
+        echo "私の名前は、".$this->name."です";
+        //文字列の間に変数やメソットを使う際には「.(ドット）」で挟む
+        //$thisは、メソットを呼び出したインスタンスに変わる
+    }
+
+    
+}
+//クラス
+//testクラスをインスタンス化して呼び出す
+//testのインスタンスを変数（＄）に代入する
+$curry= new test();
+$curry->name="haruya";
+//プロパティへのアクセス
+$menu=new test;
+//変数menuにtestクラスのインスタンスを代入
+$menu->name ="macd";
+//インスタンス->プロパティ名　でプロパティにアクセスする
+
+$curry->text();
+//textメソットを呼び出す処理
+//実行結果
+//私の名前は、haruyaです
+
+$ages=new test(3)
+//$ages=new test(引数を渡すことが出来る)
+
+
+?>
